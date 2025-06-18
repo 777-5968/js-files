f6570a5f69f12c374f239c2cfc0f0e2cbc22af02
J0khimCode <jkitavi21086@gmail.com>
2025-06-18 14:14:48 +0300

    js files

--- input.py
+++ input.py
@@ -9,5 +9,13 @@ num1=int(input("enter first number"))
 num2=int(input("enter second number"))
 sum=num1+num2
 print( "the sum is",sum)
+#get five grades and calculate the average
+grade1=int(input("enter first grade"))
+grade2=int(input("enter second grade"))
+grade3=int(input("enter third grade"))
+grade4=int(input("enter fourth grade"))
+grade5=int(input("enter fifth grade"))
+avg=(grade1+grade2+grade3+grade4+grade5)/5
+print("the average is",avg)
 
 
